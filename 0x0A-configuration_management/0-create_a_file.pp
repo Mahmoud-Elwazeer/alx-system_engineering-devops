ecc2b7b13cbc3a2a586e5804db467f022fce5e21
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-09-22 21:15:56 +0300

    test

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet, create a file in /tmp.
 
 file = { '/tmp/school':
-    ensure => '/tmp/school',
-    mode => 0744,
+    ensure => 'file',
+    mode => '0744',
     owner => 'www-data',
     group => 'www-data',
     content => 'I love Puppet',
