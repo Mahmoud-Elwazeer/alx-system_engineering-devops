ce410a58bbc3cf52823f296a9a9cff3995555806
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-24 02:30:27 +0300

    fix style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,25 +8,21 @@ import sys
 def main():
     """main function"""
     url = "https://jsonplaceholder.typicode.com/"
-    req_personal = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    req_data = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    
+    req_personal = requests.get(url + "users/{}".format(
+                sys.argv[1])).json()
+    req_data = requests.get(url + "todos", params={
+                "userId": sys.argv[1]}).json()
+
     employee_name = req_personal.get("name")
     total_tasks = len(req_data)
-    done_tasks = list(filter(lambda x:x.get("completed") is True, req_data))
+    done_tasks = list(filter(lambda x: x.get("completed") is True, req_data))
     title = [x['title'] for x in done_tasks]
     num_done = len(done_tasks)
-
 
     print("Employee {} is done with tasks \
         ({}/{}):".format(employee_name, num_done, total_tasks))
     for task in title:
         print("\t  {}".format(task))
-
-
-    # print("Employee {} is done with tasks \
-    #     ({}/{}):".format(employee_name, done_tasks, total_tasks))
-    # print("\t  {}".format(title))
 
 
 if __name__ == "__main__":
