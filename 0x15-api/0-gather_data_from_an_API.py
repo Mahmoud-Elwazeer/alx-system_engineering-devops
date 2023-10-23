7fbd54628c520faa448f7d0f5fb7d0a948c4f91d
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-24 01:10:30 +0300

    API to collect data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """import libraries"""
+import json
+import sys
+import urllib.request
+
+
+def main():
+    url = "https://jsonplaceholder.typicode.com/todos/" + sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        # content as string and readable
+        # utf_content = content.decode('utf-8')
+        # convert to dict
+        data = json.loads(content)
+        employee_name = data.get("EMPLOYEE_NAME")
+        done_tasks = data.get("NUMBER_OF_DONE_TASKS")
+        total_tasks = data.get("TOTAL_NUMBER_OF_TASKS")
+        content_task = data.get("TASK_TITLE")
+
+        print("Employee {} is done with tasks \
+            ({}/{}):".format(employee_name, done_tasks, total_tasks))
+        print("\t  {}".format(content_task))
 
 
 if __name__ == "__main__":
-    pass
+    if (len(sys.argv) != 1):
+        main()
