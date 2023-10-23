d8e45d2a96ac1139bd90b531185d00f868f030a0
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-10-24 01:55:03 +0300

    using requests

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """import libraries"""
 import json
+import requests
 import sys
-import urllib.request
 
 
 def main():
     """main function"""
-    url = "https://jsonplaceholder.typicode.com/users" + sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        # content as string and readable
-        # utf_content = content.decode('utf-8')
-        # convert to dict
-        data = json.loads(content)
-        employee_name = data.get("EMPLOYEE_NAME")
-        done_tasks = data.get("NUMBER_OF_DONE_TASKS")
-        total_tasks = data.get("TOTAL_NUMBER_OF_TASKS")
-        content_task = data.get("TASK_TITLE")
+    url = "https://jsonplaceholder.typicode.com/"
+    req_personal = requests.get(url + "users/{}".format(sys.argv[1]))
+    req_data = requests.get(url + "todos/{}".format(sys.argv[1]))
+    
+    personal_dict = json.loads(req_personal.content)
+    data_dict = json.loads(req_data.content)
+    
+    employee_name = personal_dict.get("name")
+    done_tasks = data_dict.get("userId")
+    total_tasks = data_dict.get("completed")
+    title = data_dict.get("title")
 
-        print("Employee {} is done with tasks \
-            ({}/{}):".format(employee_name, done_tasks, total_tasks))
-        print("\t  {}".format(content_task))
+
+    print("Employee {} is done with tasks \
+        ({}/{}):".format(employee_name, done_tasks, total_tasks))
+    print("\t  {}".format(title))
 
 
 if __name__ == "__main__":
