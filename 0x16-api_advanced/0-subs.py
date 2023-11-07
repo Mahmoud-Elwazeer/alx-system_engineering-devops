dbc01e62930ae30528d69afa5ad60259dd0f40d2
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-11-07 22:23:15 +0200

    fix style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,12 +11,13 @@ def number_of_subscribers(subreddit):
         # req = requests.get(url, allow_redirects=False).json()
         # get_data = req.get("data")
         # return (len(get_data.values()))
-    
+
         url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
         req = requests.get(url, allow_redirects=False).json()
         get_data = req.get("data")
-        num_subscrib = get_data.get("children")[-1].get("data").get("subreddit_subscribers")
+        num_subscrib = get_data.get("children")[-1].get(
+                "data").get("subreddit_subscribers")
         return (num_subscrib)
 
     except Exception:
