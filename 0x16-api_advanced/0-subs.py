dc2fa3f8286ac554a1b8bbb0b042a0e1f7ad1f6a
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-11-07 22:35:39 +0200

    using about

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,20 +5,24 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers"""
+
     try:
-        # url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {
+            'User-Agent': 'Mahmoud_Elwazeer'
+        }
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-        # req = requests.get(url, allow_redirects=False).json()
+        req = requests.get(url, headers=headers, allow_redirects=False).json()
+        num_subscribe = req.get("data").get("subscribers")
+        return (num_subscribe)
+
+        # url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+        # req = requests.get(url, headers=headers, allow_redirects=False).json()
         # get_data = req.get("data")
-        # return (len(get_data.values()))
-
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
-        req = requests.get(url, allow_redirects=False).json()
-        get_data = req.get("data")
-        num_subscrib = get_data.get("children")[-1].get(
-                "data").get("subreddit_subscribers")
-        return (num_subscrib)
+        # num_subscribe = get_data.get("children")[-1].get(
+        #         "data").get("subreddit_subscribers")
+        # return (num_subscrib)
 
     except Exception:
         return (0)
