b4dbd6a46616a205be34088edb6c61fcfc241772
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-11-07 22:09:03 +0200

    false redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     try:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-        req = requests.get(url).json()
+        req = requests.get(url, allow_redirects=False).json()
         get_data = req.get("data")
         return (len(get_data.values()))
 
