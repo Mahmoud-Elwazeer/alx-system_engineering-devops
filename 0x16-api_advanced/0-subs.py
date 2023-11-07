1480feb41707b7d60b15446ab0cf9e9a11e85cd9
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-11-07 22:22:19 +0200

    using hot.json

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,18 @@ import requests
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers"""
     try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        # url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+        # req = requests.get(url, allow_redirects=False).json()
+        # get_data = req.get("data")
+        # return (len(get_data.values()))
+    
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
         req = requests.get(url, allow_redirects=False).json()
         get_data = req.get("data")
-        return (len(get_data.values()))
+        num_subscrib = get_data.get("children")[-1].get("data").get("subreddit_subscribers")
+        return (num_subscrib)
 
     except Exception:
         return (0)
